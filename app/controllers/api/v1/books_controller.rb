1333f8b1f9ca076ab1a1c8858a15565160c89fd0
julie1013 <julie1380@verizon.net>
2017-04-15 13:20:09 -0400

    Added methods in books controller so that user can get to read list from back end. This is temporary.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,9 +1,29 @@
 class Api::V1::BooksController < ApplicationController
-  before_action :authenticate_user, only: [:get_info]
+  before_action :authenticate_user, only: [:get_info, :get_list, :add_to_list,
+    :remove_from_list]
 
   def index
     render json: {
       books: Book.all
+    }
+  end
+
+  def get_list
+    books = current_user.list.books
+    render json: {
+      books: books
+    }
+  end
+
+  def add_to_list
+    render json: {
+      status: "hi"
+    }
+  end
+
+  def remove_from_list
+    render json: {
+      status: "hi"
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       mount Knock::Engine => '/knock'
       get '/userinfo' => 'users#get_info'
       get '/books' => 'books#index'
+      get '/getList' => 'books#get_list'
+      post '/addToList' => 'books#add_to_list'
+      get '/removeFromList' => 'books#remove_from_list'
     end
   end
 end
